4fcd516b8c1eec1407aafa212a489e78aa3f0f42
secminhr <secminhrian@gmail.com>
2021-01-02 19:21:35 +0800

    Explode

---
+++ Assets/FrontScript/Battle/Input/Card/OnFieldDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnFieldDestroy : MonoBehaviour {
+
+    void Hide()
+    {
+        gameObject.transform.parent.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.transform.parent.GetChild(1).gameObject.SetActive(false);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+}
