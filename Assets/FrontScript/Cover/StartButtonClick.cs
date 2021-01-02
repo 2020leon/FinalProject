aac8c9cba402fb9a7c3466c4928938af7669f02a
2020leon <leon2000.tw@gmail.com>
2021-01-02 11:54:05 +0800

    Add fade in/fade out features

--- Assets/FrontScript/Cover/StartButtonClick.cs
+++ Assets/FrontScript/Cover/StartButtonClick.cs
@@ -4,8 +4,38 @@ using UnityEngine.SceneManagement;
 namespace FinalProject
 {
     public class StartButtonClick : MonoBehaviour {
+
+        private bool isFadingIn;
+        private bool isFadingOut;
+        private CanvasGroup canvasGroup;
+
+        private void Start() {
+            isFadingOut = false;
+            isFadingIn = true;
+            canvasGroup = transform.parent.GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 0f;
+        }
+
+        private void Update() {
+            if (isFadingIn && canvasGroup.alpha < 1) {
+                canvasGroup.alpha += 1f / 64f;
+            }
+            if (isFadingIn && canvasGroup.alpha == 1) {
+                isFadingIn = false;
+            }
+            if (isFadingOut && canvasGroup.alpha > 0) {
+                canvasGroup.alpha -= 1f / 128f;
+            }
+            if (isFadingOut && canvasGroup.alpha == 0) {
+                SceneManager.LoadScene("BattleScene");
+                isFadingOut = false;
+            }
+        }
+
         public void OnClick() {
-            SceneManager.LoadScene("BattleScene");
+            if (!isFadingIn && !isFadingOut) {
+                isFadingOut = true;
+            }
         }
     }
 }
