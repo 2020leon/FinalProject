7c52b19d554d5c7b72f26c9005210e1e1aa60b5a
2020leon <leon2000.tw@gmail.com>
2021-01-03 19:39:52 +0800

    Fix end turn button click

--- Assets/FrontScript/Battle/EndTurnButtonClick.cs
+++ Assets/FrontScript/Battle/EndTurnButtonClick.cs
@@ -8,7 +8,9 @@ namespace FinalProject
     {
         public void onClick()
         {
-            GameState.GiveUpInput = true;
+            if (GameState.InputState != InputState.Ready) {
+                GameState.GiveUpInput = true;
+            }
         }
     }
 }
